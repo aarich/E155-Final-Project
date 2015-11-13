1f2075977e914e9c0dbca5a2c6b0c3f3e1836eae
aerosen <arosen@hmc.edu>
2015-11-12 20:53:02 -0800

    Thursday Meeting Update
    1st draft of UART RX logic

--- fpga/Functions.sv
+++ fpga/Functions.sv
@@ -1,28 +1,42 @@
-module UARTtxrx(input  logic clk, 
-                input  logic sdi,
-                output logic sdo,
-                input  logic done,
-                output logic [7:0] cmd1,cmd2,cmd3);
+module receiveMSG(input logic clk,
+				  input logic RX,
+				  output logic[7:0] lmotor,rmotor,dur,
+				  output logic loadComplete);
+	//top level for message recive subsystem
+	//TODO: Make PLL for UART
+	logic[7:0] char;
+	logic RXdone;
+	UARTRX(clk,RX,char,RXdone)
+	always_ff @(posedge RXdone)
+		begin
+			{lmotor,rmotor,dur}={rmotor,dur,char};
+		end
+		
+endmodule
 
-    logic         sdodelayed, wasdone;
-    logic [7:0] ack;
-               
-    // assert load
-    // apply 24 clks to shift in the three commands, starting with cmd1[0]
-    // then deassert load, wait until done
-    // then apply 128 clks to shift out cyphertext, starting with cyphertext[0]
-    always_ff @(posedge clk)
-        if (!wasdone)  {ack[7:0],cmd1[7:0],cmd2[7:0],cmd3[7:0]} = {ack[7:0],cmd1[7:0],cmd2[7:0],cmd3[6:0],sdi};
-        else           {cyphertextcaptured, plaintext, key} = {cyphertextcaptured[126:0], plaintext, key, sdi}; 
-    
-    // sdo should change on the negative edge of clk
-    always_ff @(negedge clk) begin
-        wasdone = done;
-        sdodelayed = cyphertextcaptured[126];
-    end
-    
-    // when done is first asserted, shift out msb before clock edge
-    assign sdo = (done & !wasdone) ? cyphertext[127] : sdodelayed;
+module UARTRX(input logic clk,
+			  input logic RX,
+			  output logic[7:0] char,
+			  output logic done);
+	//UART RX Pin
+	logic[3:0] validCheck;
+	logic valid;
+	logic clk2;
+	logic[1:0] stopBits;
+	
+	always_ff @(posedge clk,posedge done)
+		begin
+			if(done) valid <= 0;
+			else valid <= valid | (~|(validCheck));
+		end
+	shift4 sampler(RX,clk,validCheck);
+	slowclk baudrate(clk,valid,clk2);
+	always_ff @(posedge clk2)
+		begin
+			if(valid) {done,char,stopBits}={char,stopBits,RX}; //this is an 11-bit shift register
+			else	{done,char,stopBits} = 11'h001;
+		end
+	
 endmodule
 
 module hBridgeIn(input logic pwr,done,
@@ -37,7 +51,7 @@ module pwm(input logic [6:0] power,
 	//Takes in an input signal and outputs corresponding PWM signal
 	logic [6:0] count;
 	timer #(7) pwmTimer(clk,reset,count);
-	assign wave = (power < count);
+	assign wave = (power > count);
 
 module durcheck(input logic[7:0] dur,
 				input logic clk,reset,
@@ -48,7 +62,31 @@ module durcheck(input logic[7:0] dur,
 	assign done = ~|(dur ^ durTime[29:21]);
 endmodule
 
+module shift4(input logic in,clk,
+			  output logic[3:0] out);
+	//4-register shift register, outputs all shifted bits		  
+	always_ff @(posedge clk)
+		begin
+			out[0]<=in;
+			out[1]=out[0];
+			out[2]<=out[1];
+			out[3]<=out[2];
+		end
+endmodule
 
+module slowclk(input logic clk,valid,
+			   output logic clk2);
+	//creates a second slow timer that is reliant on valid for centering
+	logic [3:0]count;
+	always_ff  @(posedge clk)
+		begin
+			if(valid) count <= count + 4'b0001; //if the signal is valid, increment the counter normally
+			else count <= 4'b0111; //if the signal is not valid, hold the slow clock right before the transition
+		end
+	assign clk2=count[3];
+endmodule
+
+	
 module timer #(parameter WIDTH=8)
 			  (input logic clk,reset,
 			   output logic [WIDTH-1:0] timeout);
